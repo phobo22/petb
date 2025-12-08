f491c9acdd21f607c89abe4fff02bcc284079f99
22127025 <ptbang22@clc.fitus.edu.vn>
2025-12-08 10:31:17 +0700

    add sorting page

---
+++ resources/views/components/sort.blade.php
@@ -0,0 +1,34 @@
+<button class="nav-link dropdown-toggle" role="button" id="pages" data-bs-toggle="dropdown" aria-expanded="false">Sorting</button>
+<ul class="dropdown-menu" aria-labelledby="pages">
+    <form method="GET" action="{{ url()->current() }}" onclick="this.submit()">
+        <input type="hidden" name="price" value="asc">
+        @foreach(request()->except(['price', 'rating']) as $key => $value)
+            <input type="hidden" name="{{ $key }}" value="{{ $value }}">
+        @endforeach
+        <li><a class="dropdown-item">Price - Low to High</a></li>
+    </form>
+
+    <form method="GET" action="{{ url()->current() }}" onclick="this.submit()">
+        <input type="hidden" name="price" value="desc">
+        @foreach(request()->except(['price', 'rating']) as $key => $value)
+            <input type="hidden" name="{{ $key }}" value="{{ $value }}">
+        @endforeach
+        <li><a class="dropdown-item">Price - High to Low</a></li>
+    </form>
+
+    <form method="GET" action="{{ url()->current() }}" onclick="this.submit()">
+        <input type="hidden" name="rating" value="asc">
+        @foreach(request()->except(['price', 'rating']) as $key => $value)
+            <input type="hidden" name="{{ $key }}" value="{{ $value }}">
+        @endforeach
+        <li><a class="dropdown-item">Rating - Low to High</a></li>
+    </form>
+    
+    <form method="GET" action="{{ url()->current() }}" onclick="this.submit()">
+        <input type="hidden" name="rating" value="desc">
+        @foreach(request()->except(['price', 'rating']) as $key => $value)
+            <input type="hidden" name="{{ $key }}" value="{{ $value }}">
+        @endforeach
+        <li><a class="dropdown-item">Rating - High to Low</a></li>
+    </form>
+</ul>
