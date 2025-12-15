1361326474bc637b58c7f76a08ad7ad7d523ddb0
22127025 <ptbang22@clc.fitus.edu.vn>
2025-12-15 10:43:39 +0700

    add user seeder

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\UserProfile;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()
+            ->count(2)
+            ->sequence(
+                ['email' => 'ptb@gmail.com', 'password' => 'ptb12345'],
+                ['email' => 'ptb1@gmail.com', 'password' => 'ptb12345'],
+            )
+            ->afterCreating(function (User $user) {
+                UserProfile::factory()->create([
+                    'user_id' => $user->id,
+                    'firstname' => $user->firstname,
+                    'lastname' => $user->lastname,
+                ]);
+            })
+            ->create();
+    }
+}
