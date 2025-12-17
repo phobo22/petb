9b39658b67fb8e2837bde70c2f627f51b1a00591
22127025 <ptbang22@clc.fitus.edu.vn>
2025-12-17 09:33:31 +0700

    align close button of cart

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,7 +95,7 @@
     </svg>  
 
     <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvasCart" aria-labelledby="My Cart">
-        <div class="offcanvas-header justify-content-center">
+        <div class="offcanvas-header justify-content-end">
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
