d2b9b3fee19a452281f825261fe05642f57d43ab
22127025 <ptbang22@clc.fitus.edu.vn>
2025-12-20 14:10:53 +0700

    add relationship of cart-item and product

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -4,15 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CartItem extends Model
 {
     use HasFactory;
-
+    
     public $timestamps = false;
     protected $fillable = [
         'cart_id',
         'product_id',
         'quantity',
     ];
+
+    public function cart(): BelongsTo {
+        return $this->belongsTo(Cart::class);
+    }
+
+    public function product(): BelongsTo {
+        return $this->belongsTo(Product::class);
+    }
 }
