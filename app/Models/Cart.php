032cf910fedd9fc4f7146318a08689bf637924ea
22127025 <ptbang22@clc.fitus.edu.vn>
2025-12-20 14:10:22 +0700

    add relationship of cart and cart-item

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cart extends Model
 {
@@ -15,5 +16,9 @@ class Cart extends Model
 
     public function user(): BelongsTo {
         return $this->belongsTo(User::class);
+    }
+
+    public function items(): HasMany {
+        return $this->hasMany(CartItem::class);
     }
 }
